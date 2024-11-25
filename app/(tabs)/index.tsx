11afa0b5f9c2c41df6256cc3dba40a085fd1aebb
Rifki Mahsyaf <rifkimahsyaf@gmail.com>
2024-11-25 19:13:18 +0700

    ubah nim

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Rifaldi Mahsyaf Azmi</Text>
+      <Text style={styles.title}>Rifaldi Mahsyaf Azmi </Text>
       <Text style={styles.title}>sistem informasi</Text>
+      <Text style={styles.title}>0702213154</Text>
     </View>
   );
 }
@@ -14,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1, // Mengisi seluruh layar
     justifyContent: 'center', // Posisi vertikal di tengah
     alignItems: 'center', // Posisi horizontal di tengah
-    backgroundColor: '#f0f8ff', // Warna latar belakang (biru muda)
+    backgroundColor: '#32CD32', // Warna latar belakang (biru muda)
   },
   title: {
     fontSize: 24, // Ukuran teks
